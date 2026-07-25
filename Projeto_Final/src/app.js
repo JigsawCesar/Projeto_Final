afbcb524be6a469fcd97b6379687bae2c0644146
JigsawCesar <felipeportela_@hotmail.com>
2026-07-25 01:28:24 -0300

    feat: adicionar rotas para gerenciamento de semestres

--- Projeto_Final/src/app.js
+++ Projeto_Final/src/app.js
@@ -4,6 +4,7 @@ import usuarioRoutes from "./routes/usuario.routes.js";
 import disciplina_routes from "./routes/disciplina.routes.js";
 import erro_middleware from "./middlewares/erro.middleware.js";
 import criar_erro from "./utils/criar_erro.js";
+import semestre_routes from "./routes/semestre.routes.js";
 
 const app = express();
 app.use(express.json());
@@ -16,7 +17,9 @@ app.use("/api/auth", authRoutes);
 
 app.use("/api/usuarios", usuarioRoutes);
 
-app.use("/api/disciplinas", disciplinaRoutes);
+app.use("/api/disciplinas", disciplina_routes);
+
+app.use("/api/semestres", semestre_routes);
 
 app.use((req, res, next) => {
   return next(criarErro("Rota não encontrada!", 404));
