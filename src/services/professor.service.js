fdadacad41e82d6dfe11c9a0dc9da8971df5c8f4
lucasstrachulski <lucasvstra@gmail.com>
2026-07-25 23:46:14 -0300

    feat(professor): adiciona criptografia com bcrypt no professor.service

--- src/services/professor.service.js
+++ src/services/professor.service.js
@@ -2,7 +2,7 @@ import ProfessorRepository from "../repositories/professor.repository.js";
 import criar_erro from "../utils/criar_erro.js";
 import bcrypt from "bcryptjs";
 
-async function cadastrarProfessor(nome, email, cpf, id) {
+async function cadastrarProfessor(nome, email, cpf, id, senha) {
   const resultadoemail = await ProfessorRepository.buscarPorEmail(email);
   const resultadonome = await ProfessorRepository.buscarPorNome(nome);
   const resultadocpf = await ProfessorRepository.buscarPorCpf(cpf);
